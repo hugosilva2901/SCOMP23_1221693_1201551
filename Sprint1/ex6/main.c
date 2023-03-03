031ad3d0ab6975458e7ecfde6aa7cddcb2ee6f07
AntonioMartingo <106592165+AntonioMartingo@users.noreply.github.com>
2023-03-03 17:41:33 +0000

    ex6, TODO b,c,d

---
+++ Sprint1/ex6/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>	
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int main () 
+{
+    int i;
+    int status;
+
+    for (i = 0; i < 4; i++)
+    {
+        if (fork () == 0) {
+            sleep (1); /*sleep(): unistd.h */
+        }
+    }
+    printf ("This is the end.\n");
+}
+
+/*
+a)How many processes will be created by this code? Justify by drawing a process tree that describes the
+processes created.
+    Resposta: Vão ser criados 16 processos.
+
+b)What change, if any, would you do to this code so that exactly 4 child processes are created?
+    Resposta: */
