2271ebf3465396f252658181ddf79d21e4e229fe
AntonioMartingo <106592165+AntonioMartingo@users.noreply.github.com>
2023-03-13 12:21:19 +0000

    12b, +ex12 Makefiles

--- Sprint1/ficha PL1A/ex12/ex12b/main.c
+++ Sprint1/ficha PL1A/ex12/ex12b/main.c
@@ -6,8 +6,34 @@
 #include <sys/wait.h>
 #include <time.h>
 
+int spawn_childs(int n) {
+    int i, indice;
+    pid_t pid;
+    int status, filho;
 
-int main(){
+    // criar n processos filhos
+    for (i = 1; i <= n; i++) {
+        pid = fork();
+        if (pid < 0) { 
+            exit(1);
+        } else if (pid == 0) { // sucesso ao criar processo filho
+            indice = i; // guardar o indice do processo filho
+            exit(indice * 2);
+        }
+    }
 
-    
+    if (pid != 0) {
+        //por cada processo filho
+        for (i = 1; i <= n; i++) {
+            filho = wait(&status); // esperar que termine
+            printf("filho %d: %d\n", filho, WEXITSTATUS(status)); 
+        }
+        return 0; 
+    }
+}
+
+int main() {
+    int indice = spawn_childs(6);
+    printf("To the parent process: %d \n", indice);
+    return 0;
 }
