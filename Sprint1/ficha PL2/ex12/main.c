146d342500673fe56a9cffb1a708db0e7e3ea3f1
hugo_silva2901 <1221693@isep.ipp.pt>
2023-04-01 17:49:28 +0100

    ex12

--- Sprint1/ficha PL2/ex12/main.c
+++ Sprint1/ficha PL2/ex12/main.c
@@ -20,28 +20,73 @@ int main()
 {
     int pipe_fdPedido[2];
     int pipe_fdPedidoChild[5][2];
-    pid_t pid;
+    pid_t pid[5];
     int status[5];
-    struct product p1;
-    struct product p2;
-    struct product p3;
-    struct product p4;
-    struct product p5;
-    strcpy("leite", p1.name);
-    strcpy("pao", p2.name);
-    strcpy("carne", p3.name);
-    strcpy("arroz", p4.name);
-    strcpy("feijao", p5.name);
-    p1.id = 1;
-    p2.id = 2;
-    p3.id = 3;
-    p4.id = 4;
-    p5.id = 5;
-    p1.price = 10;
-    p2.price = 5;
-    p3.price = 20;
-    p4.price = 15;
-    p5.price = 10;
+    struct product p[20];
+    
+    strcpy( p[0].name,"leite");
+    strcpy( p[1].name,"queijo");
+    strcpy( p[2].name,"pao");
+    strcpy( p[3].name,"carne");
+    strcpy( p[4].name,"arroz");
+    strcpy( p[5].name,"feijao");
+    strcpy( p[6].name,"frango");
+    strcpy( p[7].name,"peixe");
+    strcpy( p[8].name,"batata");
+    strcpy( p[9].name,"cenoura");
+    strcpy( p[10].name,"alface");
+    strcpy( p[11].name,"tomate");
+    strcpy( p[12].name,"cebola");
+    strcpy( p[13].name,"alho");
+    strcpy( p[14].name,"azeite");
+    strcpy( p[15].name,"vinho");
+    strcpy( p[16].name,"cerveja");
+    strcpy( p[17].name,"agua");
+    strcpy( p[18].name,"sumo");
+    strcpy( p[19].name,"refrigerante");
+
+
+    p[0].id=1;
+    p[1].id=2;
+    p[2].id=3;
+    p[3].id=4;
+    p[4].id=5;
+    p[5].id=6;
+    p[6].id=7;
+    p[7].id=8;
+    p[8].id=9;
+    p[9].id=10;
+    p[10].id=11;
+    p[11].id=12;
+    p[12].id=13;
+    p[13].id=14;
+    p[14].id=15;
+    p[15].id=16;
+    p[16].id=17;
+    p[17].id=18;
+    p[18].id=19;
+    p[19].id=20;
+    p[0].price=10;
+    p[1].price=20;
+    p[2].price=30;
+    p[3].price=40;
+    p[4].price=50;
+    p[5].price=60;
+    p[6].price=15;
+    p[7].price=25;
+    p[8].price=35;
+    p[9].price=45;
+    p[10].price=55;
+    p[11].price=65;
+    p[12].price=20;
+    p[13].price=30;
+    p[14].price=40;
+    p[15].price=50;
+    p[16].price=60;
+    p[17].price=70;
+    p[18].price=12;
+    p[19].price=22; 
+
     if (pipe(pipe_fdPedido) == -1)
     {
         perror("pipe");
@@ -58,94 +103,65 @@ int main()
 
     for (int i = 0; i < 5; i++)
     {
-        pid = fork();
+        pid[i] = fork();
         if (pid == -1)
         {
             perror("fork");
             exit(EXIT_FAILURE);
         }
 
-        if (pid == 0)
+        if (pid[i] == 0)
         {
+            
             close(pipe_fdPedido[0]);
-            close(pipe_fdPedidoChild[i][1]);
+            for (int k = 0; k < 5; k++)
+            {
+            close(pipe_fdPedidoChild[k][1]);
+            }
+            
+            int j=0;
             struct pedido pe1;
-            pe1.id = i;
+            srand( pe1.id);
+            while (j<5)
+            {
+            pe1.id= (rand() % 20) ;
             pe1.child = i;
+            write(pipe_fdPedido[1], &pe1, sizeof(pe1));
+            struct product pchild;
+            
+            read(pipe_fdPedidoChild[i][0],&pchild,sizeof(pchild));
+            printf("product id %d name %s price %d\n ",pchild.id,pchild.name,pchild.price);
+            j++;
+            }
+            close(pipe_fdPedido[1]);
+            for (int i = 0; i < 5; i++)
+            {
+                close(pipe_fdPedidoChild[i][0]);
+            }
+            exit(EXIT_SUCCESS);     
         }
     }
-    if (pid == 0)
-    {
         close(pipe_fdPedido[1]);
-        close(pipe_fdPedidoChild[0]);
-        int bet;
-        srand(bet);
-        while (1)
+            for (int i = 0; i < 5; i++)
+            {
+                close(pipe_fdPedidoChild[i][0]);
+            }
+        ssize_t n=1;
+        while (n!=0)
         {
+        struct pedido msg;
+        n = read(pipe_fdPedido[0], &msg, sizeof(msg));
 
-            int credit;
-            int signal;
-            read(pipe_fdPedido[0], &signal, sizeof(signal));
-            if (signal == 0)
-            {
-                printf("Child process terminating\n");
-                exit(EXIT_SUCCESS);
-            }
-
-            bet = (rand() % 5) + 1;
-            printf("Child process betting %d\n", bet);
-
-            write(pipe_fdPedidoChild[1], &bet, sizeof(bet));
-
-            read(pipe_fdPedido[0], &credit, sizeof(credit));
-            printf("Child process new credit: %d euros\n", credit);
+        printf("\nPedido do produto %d com o nome %s do filho %d\n", p[msg.id].id,p[msg.id].name, msg.child);
+        write(pipe_fdPedidoChild[msg.child][1], &p[msg.id], sizeof(p[msg.id]));
+        sleep(1);
         }
-        /*
-                close(pipe_fdPedido[0]);
-                close(pipe_fdPedido[1]);
-                exit(EXIT_SUCCESS);
-                */
-    }
-    else
-    {
         close(pipe_fdPedido[0]);
-        close(pipe_fdPedidoChild[1]);
-
-        while (credit > 0)
-        {
-            int number;
-            int bet;
-            srand(bet);
-            number = (rand() % 5) + 1;
-            printf("\nParent process generated %d\n", number);
-
-            int signal = 1;
-            write(pipe_fdPedido[1], &signal, sizeof(signal));
-
-            read(pipe_fdPedidoChild[0], &bet, sizeof(bet));
-            printf("Parent process read bet %d\n", bet);
-            if (number == bet)
+            for (int k = 0; k < 5; k++)
             {
-                credit += 10;
+            close(pipe_fdPedidoChild[k][1]);
             }
-            else
-            {
-                credit -= 5;
-            }
-            write(pipe_fdPedido[1], &credit, sizeof(credit));
-            sleep(1);
-        }
-
-        int signal = 0;
-        write(pipe_fdPedido[1], &signal, sizeof(signal));
-
-        wait(&status);
-
-        printf("Parent process terminating\n");
-        close(pipe_fdPedido[0]);
-        close(pipe_fdPedido[1]);
-        exit(EXIT_SUCCESS);
-    }
+    
 
     return 0;
 }
