7a03a086229a5da4d8f4166b508aa17a9d5db9cf
AntonioMartingo <106592165+AntonioMartingo@users.noreply.github.com>
2023-04-01 16:08:44 +0100

    PL2, ex9.

--- Sprint1/ficha PL2/ex9/main.c
+++ Sprint1/ficha PL2/ex9/main.c
@@ -2,84 +2,86 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <time.h>
 
 #define TAMANHO_ARRAY 50000
-#define INTERVALO_PROCURA 5000
-#define NUM_FILHOS 10
-#define MINIMO_UNIDADES_VENDIDAS 20
+#define NUMERO_FILHOS 10
+#define INTERVALO 5000
 
 typedef struct {
     int customer_code;
     int product_code;
     int quantity;
-} SaleRecord;
+} Sale;
 
-int sales[TAMANHO_ARRAY];
-int products[TAMANHO_ARRAY];
-int num_products = 0;
-
-// Funcao que procura os produtos numa determinada parte do array e tambem preenche o array com produtos
-void procurar(int indiceInicio, int indiceFim, int pipe_fd[]) {
-    int product_code;
-    for (int i = indiceInicio; i < indiceFim; i++) { // Por cada intervalo de procura
-        SaleRecord venda = {rand(), rand() % TAMANHO_ARRAY, rand() % 26}; // Cria uma venda aleatoria, com a quantidade entre 0 e 25
-        if (venda.quantity > MINIMO_UNIDADES_VENDIDAS) { // Se a quantidade vendida for maior que o minimo
-            product_code = venda.product_code;
-            write(pipe_fd[1], &product_code, sizeof(product_code)); // Escreve o codigo do produto no pipe
-        }
-    }
-    close(pipe_fd[1]);
-    return 0;
-}
+Sale sales[TAMANHO_ARRAY];
 
 int main() {
-    int pipe_fd[NUM_FILHOS][2];
-    pid_t pids[NUM_FILHOS];
-    srand(time(NULL)); // para gerar numeros aleatorios diferentes
+    srand(time(NULL));
+    // preencher o array sales com valores aleatórios
+    for (int i = 0; i < TAMANHO_ARRAY; i++) {
+        sales[i].customer_code = rand();
+        sales[i].product_code = rand() % 1000; 
+        sales[i].quantity = rand() % 22;  //se aumentar muito os possiveis valores para quantidade, vamos ter imensas entries no array de produtos com +20 vendas.
+    }
 
-    // criar os pipes e os filhos
-    for (int i = 0; i < NUM_FILHOS; i++) {
-        if (pipe(pipe_fd[i]) == -1) {
-            perror("Ero ao criar pipe");
-            return 1;
+    
+    int pipes[NUMERO_FILHOS][2]; 
+
+    // criar os pipes
+    for (int i = 0; i < NUMERO_FILHOS; i++) {
+        if (pipe(pipes[i]) == -1) {
+            perror("Erro cao criar pipe");
+           return 1;
         }
-        pid_t pid = fork();
+
+        pid_t pid = fork(); // criar um filho
+
         if (pid == -1) {
-            perror("Erro ao fazer fork");
+            perror("Errro ao criar filho");
             return 1;
         } 
-        else if (pid == 0) { // filho
-            close(pipe_fd[i][0]);  // fechar o lado de leitura do pipe, o filho so escreve no pipe
-            int indiceInicio = i * INTERVALO_PROCURA;
-            int indiceFim = indiceInicio + INTERVALO_PROCURA;
-            procurar(indiceInicio, indiceFim, pipe_fd[i]); // Chamar a funcaoi para popular e procurar o intervalo do array
-        } 
-        else { // pai
-            close(pipe_fd[i][1]);  // parent only reads from pipe
-            pids[i] = pid;
+        else if (pid == 0) { // processo filho
+            close(pipes[i][0]); // fechar o lado de leitura do pipe, filho so escreve
+
+            int inicio= i * INTERVALO; 
+            int fim = inicio+ INTERVALO;
+
+            // Verificar se a quantidade é maior que 20 e escrever no pipe
+            for (int j = inicio; j < fim; j++) {
+                Sale* sale = &sales[j];
+                if (sale->quantity > 20) { 
+                    write(pipes[i][1], &sale->product_code, sizeof(int)); // Escrever no pipe
+                }
+            }
+
+            close(pipes[i][1]); // fecha o lado de escrita do pipe
+            exit(0);
+        } else { // processo pai
+            close(pipes[i][1]); // fecha o lado de escrita do pipe
         }
     }
 
-    // o processo pai espera que todos os filhos terminem
-    for (int i = 0; i < NUM_FILHOS; i++) {
-        waitpid(pids[i], NULL, 0);
-    }
+    // declarar o array products e ler os dados do pipe
+    int products[TAMANHO_ARRAY];
+    int products_count = 0;
 
-    printf("\nProdutos que venderam mais que %d unidades numa venda:\n", MINIMO_UNIDADES_VENDIDAS);
-
-    // o processo pai le os codigos dos pipes e guarda os no produtcts
-    int product_code;
-    for (int i = 0; i < NUM_FILHOS; i++) {
-        while (read(pipe_fd[i][0], &product_code, sizeof(product_code)) > 0) { // enquanto houver codigos para ler
-            products[num_products++] = product_code; // guarda o codigo do produto no array
+    // Por cada pipe
+    for (int i = 0; i < NUMERO_FILHOS; i++) {
+        int product_code;
+        ssize_t bytes_read;
+        // Enquanto houver dados para ler
+        while ((bytes_read = read(pipes[i][0], &product_code, sizeof(int))) > 0) {
+            products[products_count++] = product_code; // Adicionar ao array o código do produto
         }
-        close(pipe_fd[i][0]); // fechar o lado de leitura do pipe
+        close(pipes[i][0]); // fecha o lado de leitura do pipe
     }
 
-    for (int i = 0; i < num_products; i++) {
-        printf("%d ", products[i]);
+    printf("Codigos de produtos com vendas superiores a 20 unidades:\n");
+    printf("Quantidade de produtos: %d\n", products_count);
+    for (int i = 0; i < products_count; i++) {
+       printf("%d ", products[i]);
     }
-    printf("\n");
+
     return 0;
 }
-
