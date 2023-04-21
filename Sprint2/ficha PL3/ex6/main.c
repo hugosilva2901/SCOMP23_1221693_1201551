744cb32974ac30f11e66677ea56e9fa0c015b2a2
hugo_silva2901 <1221693@isep.ipp.pt>
2023-04-21 17:12:53 +0100

    ex6

--- Sprint2/ficha PL3/ex6/main.c
+++ Sprint2/ficha PL3/ex6/main.c
@@ -13,62 +13,59 @@
 int main() {
     int array[1000];
     int i;
+    pid_t pid;
+    const int SIZE = 10 * sizeof(int);
     for ( i = 0; i < 1000; i++)
     {
         array[i] = rand()%1001;
     }
-    
-    /*
-  // Create an array of 1000 integers with random values between 0 and 1000
-    int arr[ARRAY_SIZE];
-    for (int i = 0; i < ARRAY_SIZE; i++) {
-        arr[i] = rand() % 1000;
+    int *shared_array;
+    int fd;
+    fd = shm_open("/main", O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR);
+    ftruncate(fd, 10*sizeof(int));
+    shared_array = mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+     if (shared_array == MAP_FAILED) {
+        perror("mmap");
+        exit(1);
     }
-
-    // Create a shared memory area to store an array of 10 integers, each containing the local maximum of 100 values
-    key_t key = IPC_PRIVATE;
-    int shmid = shmget(key, sizeof(int) * 10, IPC_CREAT | 0666);
-    int* local_maxes = (int*) shmat(shmid, NULL, 0);
-
-    // Create 10 new processes
-    for (int i = 0; i < 10; i++) {
-        int pid = fork();
-        if (pid == 0) {  // child process
-            // Determine the local maximum
-            int local_max = arr[i * BLOCK_SIZE];
-            for (int j = i * BLOCK_SIZE + 1; j < (i + 1) * BLOCK_SIZE; j++) {
-                if (arr[j] > local_max) {
-                    local_max = arr[j];
+    for (int i = 0; i < 10; i++)
+    {
+       pid = fork();
+         if (pid == -1) {
+          perror("fork");
+          exit(1);
+        } else if (pid == 0) {  
+            int temp=0;
+            for(int j=i*100;j<(i+1)*100;j++){
+                if(array[j]>temp){
+                    shared_array[i] = array[j];
+                    temp = array[j];
                 }
             }
-
-            // Store the local maximum in shared memory
-            local_maxes[i] = local_max;
+            close(fd);
             exit(0);
         }
     }
-
-    // Wait until the 10 child processes finish
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++)
+    {
         wait(NULL);
     }
-
-    // Determine the global maximum
-    int global_max = local_maxes[0];
-    for (int i = 1; i < 10; i++) {
-        if (local_maxes[i] > global_max) {
-            global_max = local_maxes[i];
+    int temp=0;
+    int GlobalMax;
+    for (int i = 0; i < 10; i++)
+    {
+        if(shared_array[i]>temp){
+            GlobalMax = shared_array[i];
+            temp = shared_array[i];
         }
     }
+    printf("GlobalMax :%d",GlobalMax);
+    // cleanup shared memory
+    munmap(shared_array, SIZE);
+    close(fd);
+    shm_unlink("/main");
 
-    printf("The biggest element in the array is %d\n", global_max);
 
-    // Eliminate the shared memory area
-    shmdt(local_maxes);
-    shmctl(shmid, IPC_RMID, NULL);
-
-    return 0;
-
-    */
+    
     return 0;
 }
